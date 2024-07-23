c8237a1e1af659f1d0e12fb65d23fee4bcdcd4eb
Emanuelle Machado <manumachado03@outlook.com>
2024-07-23 16:39:39 -0300

    feat (card component) layout and style of the card

--- src/pages/dashboard/components/CardPage.tsx
+++ src/pages/dashboard/components/CardPage.tsx
@@ -1,31 +1,32 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container } from "react-bootstrap";
+import './CardPage.css';
 
 const CardPage = () => {
   return (
-    <div className="container-card">
-      <Container fluid>
+    <div>
+      <Container className="container">
         {/* arrumar o length para a quantidade de cards a serem usados */}
         {Array.from({ length: 4 }).map((_, idx) => (
-          <Row
+          <Col sm
             key={idx}
-            style={{ border: '3px solid black', width: '14em' }}
             className="mb-2"
           >
             {/* colocar as variaveis que para o card receber as informações de icone, titulo e quantidade */}
             <Card>
               {/* colocar a variavel do icone */}
               <Card.Img
+                className="card-icon"
                 variant="top"
                 src="src\assets\images\icone-pata.png"
                 alt="icone-pata"
               />
-              <Card.Body>
+              <Card.Body className="card-text">
                 {/* colocar a variavel do titulo e quantidade */}
                 <Card.Title className="card-title">Animais</Card.Title>
                 <Card.Text className="card-quantity"> 243 </Card.Text>
               </Card.Body>
             </Card>
-          </Row>
+          </Col>
         ))}
       </Container>
     </div>
