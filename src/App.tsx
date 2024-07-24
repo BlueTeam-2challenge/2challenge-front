7d53c2c4a1089cdcdb7f57c60265d76a74346d10
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-24 17:26:28 -0300

    fix:  update colors in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { TextInput } from "./components/TextInput";
 import UserProfile from "./components/UserProfile";
+import Form from "./pages/AnimalsList/components/Form";
 import CardPage from "./pages/dashboard/components/CardPage";
 
 function App() {
   return (
     <>
-      <CardPage />
-      <TextInput
-        label="Nome"
-        type="text"
-        placeholder="Teste"
-        name="username"
-        onChange={() => console.log("Mudou")}
-      />
-      <UserProfile name="John Doe" />
+      <Form />
     </>
   );
 }
