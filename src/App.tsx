8f2634f4cc53af6985c2186030d5e732cb27e823
Emanuelle Machado <manumachado03@outlook.com>
2024-07-24 16:30:24 -0300

    feat (modal removePet) adding the component modal to remove from the formulary

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { TextInput } from "./components/TextInput";
-import UserProfile from "./components/UserProfile";
-import CardPage from "./pages/dashboard/components/CardPage";
+import { useState } from "react";
+import ModalRemove from "./components/modalRemove/ModalRemove";
 
 function App() {
+
+  {/* adicionar ao formulário a lógica - o botão no return será o botão de remover */ }
+  {/* lógica pode ser reutilizada para o modal de inserção */ }
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleDelete = () => {
+    {/* lógica para o delete */ }
+    setOpenModal(false);
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
     <>
-      <CardPage />
-      <TextInput
-        label="Nome"
-        type="text"
-        placeholder="Teste"
-        name="username"
-        onChange={() => console.log("Mudou")}
-      />
-      <UserProfile name="John Doe" />
+      <button onClick={handleOpenModal}>Excluir Item</button>
+      <ModalRemove
+        isOpen={openModal}
+        onClose={handleCloseModal}
+        onConfirm={handleDelete} />
     </>
   );
 }
