ee98a40b7c2b69a03560af9110b3eda043fd2b0d
Abner do Nascimento Santos <abnerdonantos06@gmail.com>
2024-07-28 18:22:12 -0300

    feat: add arrow button for closing sidebar

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -3,21 +3,41 @@ import NavButton from "../NavButton"
 import UserProfile from "../UserProfile"
 import { BrowserRouter as Router } from 'react-router-dom';
 import styles from './SideMenu.module.css'
-import { X } from "lucide-react";
-import { Menu } from "lucide-react";
-import { useState } from "react";
+import { X, Menu, ChevronRight, ChevronLeft } from "lucide-react";
+import { useState, useEffect } from "react";
+import { SideMenuProps } from "./types";
 
-const SideMenu = () => {
-  const [hamburger, setHamburger] = useState(false)
+const SideMenu = ({toggleHamburger, toggleMenu}: SideMenuProps) => {
+  const [hamburger, setHamburger] = useState<boolean>(false)
+  const [menu, setMenu] = useState<boolean>(false)
 
-  const toggleHamburger = () => {
+  toggleHamburger = () => {
     setHamburger(!hamburger)
   }
 
+  toggleMenu = () => {
+    setMenu(!menu)
+  }
+
+  useEffect(() => {
+    const isMobile = () => {
+      if(window.innerWidth <= 685){
+        setHamburger(true);
+        setMenu(false);
+      }else{
+        setHamburger(false)
+      }
+    };
+
+    isMobile();
+
+    window.addEventListener('resize', isMobile);
+  }, []);
+
   return (
     <Router>
-      <div className={`${styles.sidebar} ${hamburger ? styles.nav : styles.side}`}>
-      <div className={`${styles.sideContent} ${hamburger ? styles.hide : ''}`}>
+      <div className={`${styles.sidebar} ${hamburger ? styles.nav : styles.side} ${menu ? styles.sideHide : ''}`}>
+      <div className={`${styles.sideContent} ${hamburger ? styles.hide : ''} ${menu ? styles.contentHide : ''}`}>
           <Logo variant="small" />
           <UserProfile name="Karthi Madesh" />
           <div className={styles.navbar}>
@@ -26,6 +46,9 @@ const SideMenu = () => {
           </div>
         </div>
         <div className={styles.logout}><NavButton to="/" label="Logout" icon="src\assets\images\logout-icon.png" /></div>
+        <div className={`${styles.openMenu} `} onClick={toggleMenu}>
+          {menu ? <ChevronRight /> : <ChevronLeft />}
+        </div>
     </div>
     <div className={styles.menuIcon} onClick={toggleHamburger}>
       {hamburger ? <Menu /> : <X />}
