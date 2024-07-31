c783f96ba41cf16ee30a492a7bcf181668458a49
Emanuelle Machado <manumachado03@outlook.com>
2024-07-31 16:35:35 -0300

    fix (navbutton) fixing the active background button

--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -1,42 +1,41 @@
-import "./NavButton.css";
+import styles from "./NavButton.module.css";
 import { ButtonLinkProps } from "./types";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavButton = (props: ButtonLinkProps) => {
   const [hover, setHover] = useState(false);
-  // const location = useLocation();
+  const location = useLocation();
 
-  // const isActive = location.pathname === props.to;
+  const isActive = location.pathname === props.to;
 
   const buttonStyle = {
-    backgroundColor: hover ? "#26C485" : "#EEE0CB",
+    backgroundColor: isActive ? "#26C485" : "#EEE0CB",
     textDecoration: "none",
     cursor: "pointer",
   };
 
   return (
-    <div className="container">
-      {/* precisa ser adicionado as rotas no app */}
+    <div className={styles.container}>
       <Link
         to={props.to}
         style={buttonStyle}
-        className="btn"
+        className={styles.btn}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
         {props.label === "Logout" ? (
           <>
-            <p className="btn-textPage">{props.label}</p>
-            <img src={props.icon} alt="page-icon" className="btn-iconPage" />
+            <p className={styles.btnTextPage}>{props.label}</p>
+            <img src={props.icon} alt="page-icon" className={styles.btnIconPage} />
           </>
         ) : (
           <>
-            <img src={props.icon} alt="page-icon" className="btn-iconPage" />
-            <p className="btn-textPage">{props.label}</p>
+            <img src={props.icon} alt="page-icon" className={styles.btnIconPage} />
+            <p className={styles.btnTextPage}>{props.label}</p>
           </>
         )}
-      </Link>
+      </Link >
     </div>
   );
 };
