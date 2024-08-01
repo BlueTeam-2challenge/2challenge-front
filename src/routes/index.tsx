886dff7cfafb57753119002bd51349528bec9561
Weslley Silva <weslleysilva.swe@gmail.com>
2024-08-01 12:19:53 -0300

    feat: Add login page logic and route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Animals from "../pages/AnimalsList/Animals";
+import LoginLogic from "../pages/Login-Logic";
 import ProtectedRoute from "./ProtectedRoute";
 
 function Router() {
   return (
     <div className="aside">
       <Routes>
+        <Route path="/login" element={<LoginLogic />} />
         <ProtectedRoute>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/animals" element={<Animals />} />
