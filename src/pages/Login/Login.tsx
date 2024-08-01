20dafdd5d7c3b5c33925b9568759278e8f86c06a
FelipeShirae <felipeshirae@alunos.utfpr.edu.br>
2024-08-01 17:48:15 -0300

    push

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,50 @@
+import { Card, Container } from "react-bootstrap";
+import './Loginpage.css';
+import {Logo} from '../../../src/components/Logo/index'
+import {TextInput} from '../../../src/components/TextInput/index';
+import SignIn from '../../../src/components/SignIn/SignIn';
+import GoogleButton from '../../../src/components/GoogleButton/GoogleButton'
+
+const LoginPage = () => {
+
+
+  return (
+      <Container className="container">
+        
+          <Card className="card" style={{backgroundColor: 'white'}}>
+            
+            
+              <Card.Title className="card-title"><Logo variant="default"/></Card.Title>
+              <Card.Text className="card-text"> SIGN IN</Card.Text>
+              <Card.Subtitle className="card-subtitle">Enter your credentials to access your account</Card.Subtitle>
+               
+                <TextInput
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter your email"
+                />
+                <TextInput
+                    label="Password"
+                    name="passoword"
+                    type="password"
+                    placeholder="Enter your password"
+
+                />
+              
+              <SignIn 
+                label="SIGN IN"
+                isActive={true}
+              />
+              <GoogleButton
+                label="SIGN IN WITH GOOGLE"
+              />
+           
+          </Card>
+        
+      </Container>
+    
+  );
+};
+
+export default LoginPage;
